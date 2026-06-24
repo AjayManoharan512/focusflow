fe04d18981df305783305a94cc2a3c0547fc0071
Ajay Manoharan <a.manoharan@WORKTUAL.COM>
2026-06-24 11:32:17 +0530

    S

--- src/miniprojects/day9/uselocalstorage.ts
+++ src/miniprojects/day9/uselocalstorage.ts
@@ -1,13 +1,21 @@
-import react, { useEffect, useState } from "react"
+import React, { useState, useEffect } from "react"
 
 export default function useLocalStorage<T>(key: string, defaultValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
     const [value, setValue] = useState<T>(() => {
-        const saved = localStorage.getItem(key)
-        return saved ? JSON.parse(saved) : defaultValue
+        try {
+            const saved = localStorage.getItem(key)
+            return saved ? JSON.parse(saved) : defaultValue
+        } catch {
+            return defaultValue
+        }
     })
 
     useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value))
+        try {
+            localStorage.setItem(key, JSON.stringify(value))
+        } catch {
+            console.error("localStorage error")
+        }
     }, [value])
 
     return [value, setValue]
