3f0196dd8f61636a5aa64556ba32669dbd6a0c7c
Ajay Manoharan <a.manoharan@WORKTUAL.COM>
2026-07-28 11:42:12 +0530

    s

--- src/miniprojects/notesapp/index.tsx
+++ src/miniprojects/notesapp/index.tsx
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
 import styles from "../shared/styles/commonstyles.module.scss"
 import Simpledebounce from "../debounce/simpledebounce"
+import useLocalStorage from "../shared/hooks/useLocalStorage"
 
 export default function Notesapp() {
     const [modalopen, setmodalopen] = useState(false)
     const [inptchange, setinptchange] = useState("")
-    const [notes, setnotes] = useState([])
-    const [searchquery,setsearchquery]=useState("")
-   
+    const [notes, setnotes] = useLocalStorage<{ text: string, timestamp: string }[]>("notes", [])
+    const [searchquery, setsearchquery] = useState("")
+
     const today = new Date()
     const handleinptchange = (e: any) => {
-        setnotes([...notes, { text: inptchange, timestamp: new Date() }])
+        setnotes([...notes, { text: inptchange, timestamp: new Date().toISOString() }])
         setinptchange("")
         setmodalopen(false)
 
     }
-  const handleSearch = (query: string) => {
-  setsearchquery(query)
-}
-const filteredNotes = notes.filter((note: any) =>
-  note.text.toLowerCase().includes(searchquery.toLowerCase())
-)
-    
-    const handledelelist=(index:any)=>{
-        const dellist=notes.filter((_:any,i)=> i != index)
-       setnotes(dellist)
+    const handleSearch = (query: string) => {
+        setsearchquery(query)
+    }
+    const filteredNotes = notes.filter((note: any) =>
+        note.text.toLowerCase().includes(searchquery.toLowerCase())
+    )
+
+    const handledelelist = (index: any) => {
+        const dellist = notes.filter((_: any, i) => i != index)
+        setnotes(dellist)
     }
 
     return (<>
@@ -43,8 +44,24 @@ const filteredNotes = notes.filter((note: any) =>
                     {
                         filteredNotes.map((el: any, index: number) => (
                             <div key={index} className={styles.previewcard}>
-                                <div className={styles.text}>{el.text} <button onClick={()=>handledelelist(index)}>del</button></div>
-                                <span style={{ fontSize: "11px", color: "grey", display: "block", width: "100%", textAlign: "end" }}>{`${el.timestamp.toDateString()} ${el.timestamp.toLocaleTimeString([], {
+                                <div className={styles.text}>{el.text} <div className={styles.delbtn} onClick={() => handledelelist(index)}><svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                >
+                                    <path d="M3 6h18" />
+                                    <path d="M8 6V4h8v2" />
+                                    <path d="M19 6l-1 14H6L5 6" />
+                                    <path d="M10 11v6" />
+                                    <path d="M14 11v6" />
+                                </svg></div></div>
+                                <span style={{ fontSize: "11px", color: "grey", display: "block", width: "100%", textAlign: "end" }}>{`${new Date(el.timestamp).toDateString()} ${new Date(el.timestamp).toLocaleTimeString([], {
                                     hour: "2-digit",
                                     minute: "2-digit",
                                 })}`}</span>
@@ -81,7 +98,7 @@ const filteredNotes = notes.filter((note: any) =>
                                 hour: "2-digit",
                                 minute: "2-digit",
                             })}`}</span></div>
-                            
+
                         </div>
                     </div>
                 </div>
